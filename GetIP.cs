a897a4266f42f7df159e974e3393a44b44402569
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2020-12-28 20:52:30 -0700

    1st coms
    1st coms

---
+++ GetIP.cs
@@ -0,0 +1,27 @@
+/*
+ * My Program acts to Display the IP Address of a desired Machine
+ */
+using System;
+using System.Net;
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            //getting the Host Name.
+            String strHostName = string.Empty; 
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            // Using Host Name,IP address is obtained.
+            IPAddress[] addr = ipEntry.AddressList;
+ 
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {1} : ",addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
